755b70a8462e99a7d30c26470bf94ce353e348c4
ggualbertosouza <contatoguilhermegsz@gmail.com>
2026-05-11 16:17:50 -0300

    add: rabbit initial setup and connection

--- internal/rabbitmq/rabbitmq.go
+++ internal/rabbitmq/rabbitmq.go
@@ -1,10 +1,58 @@
 package rabbitmq
 
 import (
+	"log"
+
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
 type RabbitMq struct {
 	Connection *amqp.Connection
 	Channel    *amqp.Channel
+}
+
+func New(url string) *RabbitMq {
+	conn, err := amqp.Dial(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ch, err := conn.Channel()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &RabbitMq{
+		Connection: conn,
+		Channel:    ch,
+	}
+}
+
+func (r *RabbitMq) Setup(exchange, queue, routingKey string) error {
+	if err := r.DeclareExchange(exchange); err != nil {
+		return err
+	}
+
+	if err := r.DeclareQueue(queue); err != nil {
+		return err
+	}
+
+	if err := r.BindQueue(queue, routingKey, exchange); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *RabbitMq) DeclareExchange(name string) error {
+	return r.Channel.ExchangeDeclare(name, "direct", true, false, false, false, nil)
+}
+
+func (r *RabbitMq) DeclareQueue(name string) error {
+	_, err := r.Channel.QueueDeclare(name, true, false, false, false, nil)
+	return err
+}
+
+func (r *RabbitMq) BindQueue(queue, routingKey, exchange string) error {
+	return r.Channel.QueueBind(queue, routingKey, exchange, false, nil)
 }
--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"log"
+
 	"github.com/ggualbertosouza/go-rabbitMq/internal/config"
+	"github.com/ggualbertosouza/go-rabbitMq/internal/rabbitmq"
 	"github.com/ggualbertosouza/go-rabbitMq/internal/server"
 )
 
@@ -9,11 +12,14 @@ func main() {
 	config.LoadEnv()
 
 	hc := config.HttpConfig()
+	rc := config.RabbitMQConfig()
 
-	s := server.Server{
-		Port: hc.Port,
+	rabbit := rabbitmq.New(rc.URL)
+	if err := rabbit.Setup(rc.Exchange, rc.Queue, rc.RoutingKey); err != nil {
+		log.Fatal(err)
 	}
-	r := server.NewRouter()
 
+	s := server.Server{Port: hc.Port}
+	r := server.NewRouter()
 	s.Init(r)
 }
